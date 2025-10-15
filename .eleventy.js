1ac8db6b29b80dd355089c9278e69cb01c83646b
dzyy3 <warrendaisym@gmail.com>
2025-10-14 23:21:12 -0700

    Remove submodule reference for collectionProjectEleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,24 @@
 module.exports = function(eleventyConfig) {
-  // Tell Eleventy to copy the entire styles folder
+  // Copy static assets
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("assets");
 
+  // create a variable for our dev environment; production = gh-pages, nothing = local by default
+  const isProduction = process.env.ELEVENTY_ENV === "production";
+
+  // Global variable for our path prefix, remember to insert your repo name
+  eleventyConfig.addGlobalData("pathPrefix", isProduction ? "/collections/" : "/");
+
+  // create a filter for our url prefix 
+  eleventyConfig.addFilter("prefixedUrl", (url, prefix) => {
+    const finalPrefix = prefix || (isProduction ? "/collections/" : "/");
+    return finalPrefix + url.replace(/^\/+/, "");
+  });
+
+	//tells eleventy to output to docs instead of _site
   return {
-    dir: {
-      input: ".",
-      output: "_site"
-    }
+    dir: { input: ".", output: "docs" },
+    htmlTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"]
   };
 };
